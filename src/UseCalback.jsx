6d07dd1ac65769949f15b2210fceff849d86b14b
khafidhfuadi <48312440+Khafidhfuadi@users.noreply.github.com>
2020-09-26 08:06:17 +0700

    usecallback

---
+++ src/UseCalback.jsx
@@ -0,0 +1,32 @@
+import React, { useCallback } from "react";
+import { Fragment } from "react";
+
+const BelajaruseCallback = (props) => {
+  const [kelas, setKelas] = React.useState([]);
+  const jumlahKelas = useCallback(() => {
+    return ["X TKJ", "X RPL", "XI TKJ", "XI RPL"];
+  }, []);
+
+  React.useEffect(() => {
+    let data = jumlahKelas();
+    setKelas(data);
+    console.log("effect di panggil");
+  }, [jumlahKelas]);
+  return (
+    <Fragment>
+      <div className="container mt-3">
+        <h2>Latihan useCallback</h2>
+        <ul className="list-group mt-3">
+          <li className="list-group-item active">Banyaknya kelas</li>
+          {kelas.map((kelas, index) => (
+            <li className="list-group-item">
+              {index + 1}. {kelas}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </Fragment>
+  );
+};
+
+export default BelajaruseCallback;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Home from "./UseState";
+import Home from "./UseCalback";
 
 // import App from "./App";
 
